0ff05600dde7cd77e129fa2d43352d9bb8ab4075
garthvh <garthvh@yahoo.com>
2016-08-07 08:33:40 -0700

    fix indent

--- vend.py
+++ vend.py
@@ -120,7 +120,7 @@ def nightrider(stick, color, wait_ms=70):
 # Main program logic follows:
 if __name__ == '__main__':
 
-	# Create NeoPixel object with appropriate configuration.
+    # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
     # Intialize the library (must be called once before other functions).
     strip.begin()
@@ -130,24 +130,24 @@ if __name__ == '__main__':
     # Initialize library.
     disp.begin(contrast=40)
 
-	print ('Press Ctrl-C to quit.')
-	while True:
-		# Color wipe animations.
-		colorWipe(strip, Color(255, 0, 0))      # Red wipe
-        	colorWipe(strip, Color(255, 255, 255))  # White wipe
-        	colorWipe(strip, Color(0, 255, 0))      # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))      # Green wipe
-		# Theater chase animations.
-		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-		# Rainbow animations.
-		rainbow(strip)
-		rainbowCycle(strip)
-		theaterChaseRainbow(strip)
-        	# Cylon
-        	for t in range (0, 10,1):
-                	nightrider(strip, Color(255,0,0), 65)
- 
-        	for t in range (0, 10,1):
-                	nightrider(strip, Color(0,0,255), 65)
+    print ('Press Ctrl-C to quit.')
+    while True:
+        # Color wipe animations.
+        colorWipe(strip, Color(255, 0, 0))      # Red wipe
+            colorWipe(strip, Color(255, 255, 255))  # White wipe
+            colorWipe(strip, Color(0, 255, 0))      # Blue wipe
+        colorWipe(strip, Color(0, 0, 255))      # Green wipe
+        # Theater chase animations.
+        theaterChase(strip, Color(127, 127, 127))  # White theater chase
+        theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+        theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+        # Rainbow animations.
+        rainbow(strip)
+        rainbowCycle(strip)
+        theaterChaseRainbow(strip)
+            # Cylon
+            for t in range (0, 10,1):
+                    nightrider(strip, Color(255,0,0), 65)
+
+            for t in range (0, 10,1):
+                    nightrider(strip, Color(0,0,255), 65)
