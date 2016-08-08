7a1498a5ff64aaf6f33257b48cf7e73aaf74d6f1
garthvh <garthvh@yahoo.com>
2016-08-08 07:56:59 -0700

    clean up vending file

--- vend.py
+++ vend.py
@@ -7,6 +7,8 @@
 #           Nokia 5110 Screen 84x48
 #           4 16mm Red Push Buttons
 #           4 Parallax Continuous Rotation Servos
+#           16 Channel 12 bit pwm Servo Motor Driver i2c Module Board
+#           Step-down Voltage Regulator 
 #
 # Built with with code from 
 # TM1803 LEDs https://github.com/jgarff/rpi_ws281x
@@ -42,6 +44,9 @@ DC = 23
 RST = 24
 SPI_PORT = 0
 SPI_DEVICE = 0
+# LCD Size Variables
+LCD_WIDTH = 84;
+LCD_HEIGHT = 48;
 
 # Push Button Pin Assignments
 PUSH_BUTTON_A = 6  # GPIO pin for Push Button A (other end to GND)
@@ -49,14 +54,11 @@ PUSH_BUTTON_B = 13 # GPIO pin for Push Button B (other end to GND)
 PUSH_BUTTON_C = 19 # GPIO pin for Push Button C (other end to GND)
 PUSH_BUTTON_D = 26 # GPIO pin for Push Button D (other end to GND)
 
-LCD_WIDTH = 84;
-LCD_HEIGHT = 48;
-
 # Define functions to change the display on screen
 def changeScreenText(draw, font, line1, line2, line3, line4):
-    """ Adjust the text displayed on the screen ~15 Characters per line """
+    """ Adjust the text displayed on the screen ~12 Characters per line """
     # Draw Empty rectangle on the screen
-    draw.rectangle((0,0,LCD_WIDTH,LCD_HEIGHT), outline=255, fill=255)
+    draw.rectangle((0, 0, LCD_WIDTH, LCD_HEIGHT), outline=255, fill=255)
     # Write the text out for each line
     draw.text((0, 0),  line1, font=font)
     draw.text((0, 12), line2, font=font)
@@ -66,7 +68,7 @@ def changeScreenText(draw, font, line1, line2, line3, line4):
     disp.image(image)
     disp.display()
 
-# Define functions which animate LEDs in various ways.
+# Define functions to animate TM1803 LEDs
 def colorWipe(strip, color, wait_ms=50):
 	"""Wipe color across display a pixel at a time."""
 	for i in range(strip.numPixels()):
@@ -97,8 +99,8 @@ def wheel(pos):
 		return Color(0, pos * 3, 255 - pos * 3)
         
 def dimColor (color):
-        """ Color is an 32-bit int that merges the values into one """
-        return (((color&0xff0000)/3)&0xff0000) + (((color&0x00ff00)/3)&0x00ff00) + (((color&0x0000ff)/3)&0x0000ff)
+    """ Color is an 32-bit int that merges the values into one """
+    return (((color&0xff0000)/3)&0xff0000) + (((color&0x00ff00)/3)&0x00ff00) + (((color&0x0000ff)/3)&0x0000ff)
 
 def rainbow(strip, wait_ms=20, iterations=1):
 	"""Draw rainbow that fades across all pixels at once."""
@@ -128,28 +130,28 @@ def theaterChaseRainbow(strip, wait_ms=50):
 				strip.setPixelColor(i+q, 0)
                 
 def nightrider(stick, color, wait_ms=70):
-        """ kitt / cylon scanner """
-        for i in range(stick.numPixels()):
-                stick.setPixelColor(i,color)
-                stick.setPixelColor(i-1,dimColor(color))
-                stick.show()
-                time.sleep(wait_ms/1000.0)
-                stick.setPixelColor(i,0)
-                stick.setPixelColor(i-1,0)
-        # reverse the direction
-        for i in xrange((stick.numPixels())-1,-1,-1):
-                stick.setPixelColor(i,color)
-                stick.setPixelColor(i+1,dimColor(color))
-                stick.show()
-                time.sleep(wait_ms/1000.0)
-                stick.setPixelColor(i,0)
-                stick.setPixelColor(i+1,0)
+    """ kitt / cylon scanner """
+    for i in range(stick.numPixels()):
+            stick.setPixelColor(i,color)
+            stick.setPixelColor(i-1,dimColor(color))
+            stick.show()
+            time.sleep(wait_ms/1000.0)
+            stick.setPixelColor(i,0)
+            stick.setPixelColor(i-1,0)
+    # reverse the direction
+    for i in xrange((stick.numPixels())-1,-1,-1):
+            stick.setPixelColor(i,color)
+            stick.setPixelColor(i+1,dimColor(color))
+            stick.show()
+            time.sleep(wait_ms/1000.0)
+            stick.setPixelColor(i,0)
+            stick.setPixelColor(i+1,0)
 
 
 # Main program logic follows:
 if __name__ == '__main__':
 
-    # Stuff You Do Once
+    ###  Stuff You Do Once  ###
     # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
     # Intialize the library (must be called once before other functions).
@@ -164,17 +166,15 @@ if __name__ == '__main__':
     GPIO.setup(PUSH_BUTTON_C, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(PUSH_BUTTON_D, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     
-    # Width and Height Variables
+    # Set Width and Height Variables
     LCD_WIDTH = LCD.LCDWIDTH
     LCD_HEIGHT = LCD.LCDHEIGHT
-    
     # Start the Nokia Screen using Hardware SPI:
     disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
     # Initialize library.
-    disp.begin(contrast=40)
+    disp.begin(contrast=45)
     # Load default font.
-    #font = ImageFont.load_default()
-    
+    # font = ImageFont.load_default()
     # Some nice fonts to try: http://www.dafont.com/bitmap.php	
     font = ImageFont.truetype('fonts/DroidSansMono.ttf', 12)
     # Create Empty Image Object
@@ -188,11 +188,11 @@ if __name__ == '__main__':
     colorWipe(strip, Color(255, 255, 255))  # White wipe
     colorWipe(strip, Color(0, 255, 0))      # Blue wipe
     colorWipe(strip, Color(0, 0, 255))      # Green wipe
-    
-    
+    # Print Some text and button pushs to the console
     print ('Garth and Jason made this super fancy ')
     print ('Alexa Powered Pi Vending App that is running')
     print ('Press Ctrl-C to quit.')
+    # Loop
     while True:
         # Flip back to white lights after the button action has run
         colorWipe(strip, Color(255, 255, 255))   # White wipe
@@ -206,7 +206,7 @@ if __name__ == '__main__':
         if button_a_state == False:
             print('Button A Pressed')
             # Write some text
-            changeScreenText(draw, font, "YOU PRESSED", "  BUTTON A", " ENJOY THE", "   CYLON")
+            changeScreenText(draw, font, "YOU PRESSED", " BUTTON A", " ENJOY THE", "   CYLON")
             # Night Rider / Cylon
             for t in range (0, 10,1):
                 nightrider(strip, Color(255,0,0), 65)
@@ -222,7 +222,7 @@ if __name__ == '__main__':
             box_width = ((LCD.LCDWIDTH / 2) -4)
             box_height = ((LCD.LCDHEIGHT / 2) -4)
             # Write some text
-            changeScreenText(draw, font, "YOU PRESSED", "  BUTTON B", " ENJOY THE", "  RAINBOWS")
+            changeScreenText(draw, font, "YOU PRESSED", " BUTTON B", " ENJOY THE", "  RAINBOWS")
             # Draw Button Images
             # A Button 2,2,22,22
             #draw.rectangle((2,2, box_width, box_height), outline=0, fill=255)
@@ -236,7 +236,7 @@ if __name__ == '__main__':
         if button_c_state == False:
             print('Button C Pressed')
             # Write some text
-            changeScreenText(draw, font, "YOU PRESSED", "  BUTTON C", " ENJOY THE", "RAINBOW CYCLE")
+            changeScreenText(draw, font, "YOU PRESSED", " BUTTON C", " ENJOY THE", "RAINBOW CYCLE")
             rainbow(strip)
             rainbowCycle(strip)
             time.sleep(0.2)
@@ -246,7 +246,7 @@ if __name__ == '__main__':
         if button_d_state == False:
             print('Button D Pressed')
             # Write some text
-            changeScreenText(draw, font, "YOU PRESSED", "  BUTTON D", "ENJOY THE RBW", " THEATER CHASE")
+            changeScreenText(draw, font, "YOU PRESSED", " BUTTON D", "ENJOY THE RBW", " THEATER CHASE")
             # Theater chase animations.
             theaterChase(strip, Color(127,   0,   0))  # Red theater chase
             theaterChase(strip, Color(127, 127, 127))  # White theater chase
